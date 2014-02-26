4a2921be19ad6723c51395ba150d0e645fc492bc
Andres Garcia <andres@zemoga.com>
2014-02-26 12:41:04 -0500

    Update base-idea.js
    Adding a remove attr observer function

--- base-idea.js
+++ base-idea.js
@@ -1,55 +1,84 @@
-        /**
-         * THIS FUNCTION IS NOT THE FINAL POLYFILL... is just a simple function containing a base idea!!!!!!!!!
-         * 
-         * What's different here??? -> Intended ti support ALSO IE8
-         *
-         *
-         * Register attribute changes in the DOM (yes... should be a plugin)
-         * @author Andres Garcia oagarciar@gmail.com
-         * @param  {HTMLElement} element DOM where the event/observer is tied
-         * @param  {Function} callback Event to be fired once attibute is changed
-         */
-        function registerDOMEventChangeAttr(element, callback){
-            var div = document.createElement('div'),
-                MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
-                DOMAttrModifiedSupport,
-                onPropertyChangeSupport;
-                
-            //IE9, FF
-            if(div.addEventListener){
-                div.addEventListener('DOMNodeInserted', function(){ DOMAttrModifiedSupport = true; });
-                div.appendChild(div.cloneNode(true));
+/**
+ * This constructor function is not a Polyfill!!! Is just a base idea of the future polyfill
+ * Register attribute changes in the DOM
+ * @author Andres Garcia, Zemoga Inc https://github.com/oagarcia/MutationObserverPolyfill
+ * @param  {HTMLElement} element DOM where the event/observer is tied
+ * @param  {Function} callback Event to be fired once attibute is 
+ * @constructor
+ */
+sen.MutationObserverChangedAttr = function(element, callback){
+    var div = document.createElement('div');
+
+        this.propertyChangeCallback = function(){
+            callback.call(element);
+        };
+
+        this.MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+        this.DOMAttrModifiedSupport = null;
+        this.onPropertyChangeSupport = null;
+        this.observer = null;
+
+        
+        
+    //IE9, FF
+    if(div.addEventListener){
+        div.addEventListener('DOMNodeInserted', function(){ this.DOMAttrModifiedSupport = true; });
+        div.appendChild(div.cloneNode(true));
+    }
+    
+    //Old IE (8-)
+    onPropertyChangeSupport = typeof div.onpropertychange === 'object';
+
+
+    //Great browsers
+    if(this.MutationObserver){
+        var MutationObserverChangedAttrInstance = this;
+        this.observer = new this.MutationObserver(function( mutations ) {
+            if(mutations[0].type === 'attributes'){
+                MutationObserverChangedAttrInstance.propertyChangeCallback();
+            }
+        });
+        this.observer.observe(element, { attributes: true, childList: true, characterData: true });
+
+    //IE9, FF 
+    }else if(this.DOMAttrModifiedSupport){
+        element.addEventListener('DOMAttrModified', this.propertyChangeCallback, false);
+
+    //Old IE (8-)
+    }else if(this.onPropertyChangeSupport){
+        if(element.attachEvent){
+            element.attachEvent('onpropertychange', this.propertyChangeCallback);
+        }else{
+            element.onpropertychange = this.propertyChangeCallback;
+        }
+    }else{
+        //I'm in problems... no DOM events supported at all! Set interval???... However I don't need to support tooooo old browsers.
+    }
+
+    element.MutationObserverInstance = this;
+};
+
+/**
+ * Removes already assigned RegisterMutationObserverChangedAttr
+ * @param  {HTMLElement} element DOM element with observer assigned
+ */
+sen.removeMutationObserverChangedAttr = function (element){
+    var obs = element.MutationObserverInstance;
+    if(obs){
+        if(obs.observer){
+            obs.observer.disconnect();
+        }else if(obs.DOMAttrModifiedSupport){
+            element.removeEventListener('DOMAttrModified', obs.propertyChangeCallback, false);
+        }else if(obs.onPropertyChangeSupport){
+            if(element.attachEvent){
+                element.detachEvent('onpropertychange', obs.propertyChangeCallback);
+            }else{
+                element.onpropertychange = null;
             }
             
-            //Old IE (8-)
-            onPropertyChangeSupport = typeof div.onpropertychange === 'object';
-
-            //Great browsers
-            if(MutationObserver){
-                var observer = new MutationObserver(function( mutations ) {
-                    if(mutations[0].type === 'attributes'){
-                        callback.call(element);
-                        //observer.disconnect();
-                    }
-                });
-
-            observer.observe(element, { attributes: true, childList: true, characterData: true });
-            }else if(DOMAttrModifiedSupport){
-                element.addEventListener('DOMAttrModified', function (event) {
-                    //console.log('done in DOMAttrModified');
-                    callback.call(element);
-                }, false);
-            }else if(onPropertyChangeSupport){
-                if(element.attachEvent){
-                    element.attachEvent('onpropertychange', function (event) {
-                        callback.call(element);
-                    });
-                }else{
-                    element.onpropertychange = function () {
-                        callback.call(element);
-                    };
-                }
-            }else{
-                //I'm in problems... no DOM events supported at all! Set interval???... However I don't need to support tooooo old browsers.
-            }
+        }else{
+            //I'm in problems... no DOM events supported at all! Set interval???... However I don't need to support tooooo old browsers.
         }
+    }
+    element.MutationObserverInstance = undefined;
+};
